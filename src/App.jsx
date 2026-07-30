9d3210b8e484a91b0e911b553d7d1106f5feaae3
X-N-aditi <aditishukla564@gmail.com>
2026-07-30 22:06:22 +0530

    react folder setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Image convertor</h1>
+
+      <p>Welcome to Image convertor, Please find your refrence here.</p>
+    </>
+  )
+}
+
+export default App
